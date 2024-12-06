d11aa2d6c7dd34580431e09ec6c4bca48fe0f07f
M Rizwan <m.rizwan@example.com>
2024-12-06 18:55:39 +0500

    first commit

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,143 +1,106 @@
-import { React, useState } from "react";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-import styles from "../../styles/styles";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { server } from "../../server";
-import { toast } from "react-toastify";
+ 
+import React, { useState } from "react";
+import Left from "../../Assests/images/robot.jpg";
+import { Link, } from "react-router-dom";
+import { CiMail } from "react-icons/ci";
+import { FaFacebook } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { CiLock } from "react-icons/ci";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
-const Login = () => {
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [visible, setVisible] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    await axios
-      .post(
-        `${server}/user/login-user`,
-        {
-          email,
-          password,
-        },
-        { withCredentials: true }
-      )
-      .then((res) => {
-        toast.success("Login Success!");
-        navigate("/");
-        window.location.reload(true); 
-      })
-      .catch((err) => {
-        toast.error(err.response.data.message);
-      });
-  };
-
+function Login() {
+  const [showPass, setShowPass] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Login to your account
-        </h2>
-      </div>
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email address
-              </label>
-              <div className="mt-1">
-                <input
-                  type="email"
-                  name="email"
-                  autoComplete="email"
-                  required
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
+    <div className="flex w-screen h-screen overflow-hidden" style={{ backgroundImage: `url(${Left})`, backgroundSize: "cover" }}>
+      <div className="h-full my-10 flex items-center justify-center relative" style={{ backgroundImage: `url(${Left})`, backgroundSize: "cover" }}>
+        <div className="min-h-screen  p-10 justify-center mx-auto">
+          <div className="w-full max-w-xl mx-auto p-4">
+            <p className="text-white text-2xl md:text-3xl font-bold">Welcome to</p>
+            <p className="text-white text-2xl md:text-3xl font-bold">Teach Support Agent!</p>
+            <p className="text-white font-bold">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+            </p>
+          </div>
+        </div>
+        <div className="h-full flex items-center justify-center">
+          <div className="h-full w-full relative">
+            <div className="p-6 flex justify-center mx-auto rounded-xl hover:scale-105 transition duration-300" style={{ height: "300px" }}>
+              <div className="flex flex-col shadow-xl justify-center bg-white  rounded-xl p-6 border-4 hover:scale-105 transition duration-300" style={{ borderTopColor: "#003942", borderBottomColor: "#003942", width: "600px", height: "650px" }}>
+                <div className=" space-y-6 text-2xl md:text-3xl text-gray-900 text-center font-bold flex justify-center">
+                  Create an account
+                </div>
+                <p className="p-4 flex items-center justify-center">
+                  <span className="pe-2 text-black text-sm">Already have an Account?</span>
+                  <Link to="/signup" className="font-semibold underline text-sm text-gray-900">Login</Link>
+                </p>
+                <div>
+                  <form className="bg-white">
+                    <label className="block mb-4">
+                    <Link to="https://accounts.google.com/signin" target="_blank" rel="noopener noreferrer">
+
+                      <div className="mt-3 text-lg md:text-xl text-gray-600 text-center font-bold flex justify-center pl-12 w-full p-2 py-3 border bg-white focus:bg-gray-200 rounded-[50px] focus:border-sky-600 focus:outline-sky-600 bg-opacity-80">
+                        <FcGoogle className="mt-1 w-6 h-6 mr-2 text-gray-400 hover:shake" />
+                        Create account with Google
+                      </div>
+                      </Link>
+                      <Link to="https://www.facebook.com/login" target="_blank" rel="noopener noreferrer">
+
+                      <div className="mt-3 text-lg md:text-xl text-gray-600 text-center font-bold flex justify-center pl-12 w-full p-2 py-3 border bg-white focus:bg-gray-200 rounded-[50px] focus:border-sky-600 focus:outline-sky-600 bg-opacity-80">
+                        <FaFacebook className="mt-1 w-6 h-6 mr-2 text-blue-500 hover:shake" />
+                        Create account with Google
+                      </div>
+                      </Link> 
+                      <div className="flex items-center my-4">
+                        <hr className="flex-grow border-gray-300" />
+                        <span className="mx-4 text-gray-500">or</span>
+                        <hr className="flex-grow border-gray-300" />
+                      </div>
+                    </label>
+                    <span className="font-semibold mb-4 ">Enter your email address to create an account</span>
+
+                    <label className="block mb-4 ">
+                      <span className="font-semibold mb-2 text-gray-600">Your Email</span>
+                      <div className="flex relative items-center">
+                        <CiMail className="absolute left-3 w-6 h-6 mr-2 text-gray-400 hover:shake" />
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="pl-12 w-full p-2 py-3 border bg-white focus:bg-gray-200 rounded-md focus:border-sky-600 focus:outline-sky-600 bg-opacity-80" placeholder="Enter your email address"
+                        required />
+                      </div>
+                    </label>
+                    <label className="block mb-4">
+                      <span className="font-semibold mb-2 text-[#2ca1cb]"> Password</span>
+                      <div className="flex relative items-center">
+                        {showPass ? (
+                          <FaEyeSlash className="absolute right-1 w-6 h-6 mr-2 text-gray-500 hover:shake" onClick={() => setShowPass(!showPass)} />
+                        ) : (
+                          <FaEye className="absolute right-1 w-6 h-6 mr-2 text-gray-500 hover:shake" onClick={() => setShowPass(!showPass)} />
+                        )}
+                        <CiLock className="absolute left-2 w-6 h-6 mr-2 text-gray-400 hover:shake" />
+                        <input type={showPass ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)} className="pl-12 w-full p-2 py-3 border bg-white focus:bg-gray-200 rounded-md focus:border-sky-600 focus:outline-sky-600 bg-opacity-80" placeholder="Create your password"
+                        required />
+                      </div>
+                    </label>
+
+
+                    <button id="openModalBtn" className="flex justify-center mx-auto rounded-xl px-4 py-2 my-3 overflow-hidden relative group bg-gray-500 cursor-pointer border-2 font-medium text-lg border-gray-500 w-3/5 text-white hover:scale-105 duration-[900ms]">
+                      <span className="relative ">
+                        Create an account
+                      </span>
+                    </button>
+
+                  </form>
+                </div>
               </div>
             </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <div className="mt-1 relative">
-                <input
-                  type={visible ? "text" : "password"}
-                  name="password"
-                  autoComplete="current-password"
-                  required
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-                {visible ? (
-                  <AiOutlineEye
-                    className="absolute right-2 top-2 cursor-pointer"
-                    size={25}
-                    onClick={() => setVisible(false)}
-                  />
-                ) : (
-                  <AiOutlineEyeInvisible
-                    className="absolute right-2 top-2 cursor-pointer"
-                    size={25}
-                    onClick={() => setVisible(true)}
-                  />
-                )}
-              </div>
-            </div>
-            <div className={`${styles.noramlFlex} justify-between`}>
-              <div className={`${styles.noramlFlex}`}>
-                <input
-                  type="checkbox"
-                  name="remember-me"
-                  id="remember-me"
-                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                />
-                <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  Remember me
-                </label>
-              </div>
-              <div className="text-sm">
-                <a
-                  href=".forgot-password"
-                  className="font-medium text-blue-600 hover:text-blue-500"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
-              >
-                Submit
-              </button>
-            </div>
-            <div className={`${styles.noramlFlex} w-full`}>
-              <h4>Not have any account?</h4>
-              <Link to="/sign-up" className="text-blue-600 pl-2">
-                Sign Up
-              </Link>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Login;
+ 
--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,184 +1,121 @@
-import { React, useState } from "react";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-import styles from "../../styles/styles";
-import { Link } from "react-router-dom";
-import { RxAvatar } from "react-icons/rx";
-import axios from "axios";
-import { server } from "../../server";
-import { toast } from "react-toastify";
 
-const Singup = () => {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [visible, setVisible] = useState(false);
-  const [avatar, setAvatar] = useState(null);
+import React, { useState } from "react";
+import robot from "../../Assests/images/robot.jpg";
+import { Link, } from "react-router-dom";
+import { ToastContainer, } from "react-toastify";
 
-  const handleFileInputChange = (e) => {
-    const reader = new FileReader();
+import { CiMail } from "react-icons/ci";
+import { CiLock } from "react-icons/ci";
+import { FiUser } from "react-icons/fi";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        setAvatar(reader.result);
-      }
-    };
 
-    reader.readAsDataURL(e.target.files[0]);
-  };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    axios
-      .post(`${server}/user/create-user`, { name, email, password, avatar })
-      .then((res) => {
-        toast.success(res.data.message);
-        setName("");
-        setEmail("");
-        setPassword("");
-        setAvatar();
-      })
-      .catch((error) => {
-        toast.error(error.response.data.message);
-      });
-  };
+function Signup() {
+  const [showPass, setShowPass] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Register as a new user
-        </h2>
-      </div>
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Full Name
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="text"
-                  autoComplete="name"
-                  required
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
+    <div className="flex w-screen h-screen overflow-hidden" style={{ backgroundImage: `url(${robot})`, backgroundSize: "cover" }}>
+      <ToastContainer />
+      <div className="h-full my-10 flex items-center justify-center relative" >
+        <div className="min-h-screen  p-10 justify-center mx-auto">
+          <div className="w-full max-w-xl mx-auto p-4">
+            <p className="text-white text-2xl md:text-3xl font-bold">Welcome to</p>
+            <p className="text-white text-2xl md:text-3xl font-bold">Teach Support Agent!</p>
+            <p className="text-white font-bold">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+            </p>
+          </div>
+        </div>
+        <div className="h-full flex items-center justify-center">
+          <div className="h-full w-full relative">
+            <div className="p-6 flex justify-center mx-auto rounded-xl hover:scale-105 transition duration-300" style={{ height: "300px" }}>
+              <div className="flex flex-col shadow-xl justify-center bg-white  rounded-xl p-6 border-4 hover:scale-105 transition duration-300" style={{ borderTopColor: "#003942", borderBottomColor: "#003942", width: "600px", height: "650px" }}>
+                <div className=" space-y-6 text-2xl md:text-3xl text-gray-900 text-center font-bold flex justify-center">
+                  Create an account
+                </div>
+                <div>
+                  <form className="bg-white">
+
+                    <label className="block mb-4">
+                      <Link to="https://accounts.google.com/signin" target="_blank" rel="noopener noreferrer">
+
+                        <div className="mt-3 text-lg md:text-xl text-gray-600 text-center font-bold flex justify-center pl-12 w-full p-2 py-3 border bg-white focus:bg-gray-200 rounded-md focus:border-sky-600 focus:outline-sky-600 bg-opacity-80">
+                          Create account with Google
+                          <FcGoogle className="mt-1 w-6 h-6 ml-2 text-gray-400 hover:shake" onClick={() => setShowPass(!showPass)} />
+                        </div>
+                      </Link>
+                      <div className="flex items-center my-4">
+                        <hr className="flex-grow border-gray-300" />
+                        <span className="mx-4 text-gray-500">or</span>
+                        <hr className="flex-grow border-gray-300" />
+                      </div>
+                    </label>
+                    <label className="block mb-4 ">
+                      <span className="font-semibold mb-2 text-[#2ca1cb]"> Email Address</span>
+                      <div className="flex relative items-center">
+                        <CiMail className="absolute left-3 w-6 h-6 mr-2 text-gray-400 hover:shake" />
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="pl-12 w-full p-2 py-3 border bg-white focus:bg-gray-200 rounded-md focus:border-sky-600 focus:outline-sky-600 bg-opacity-80" placeholder="Enter your email address" 
+                        required/>
+                      </div>
+                    </label>
+                    <label className="block mb-4">
+                      <span className="font-semibold mb-2 text-[#2ca1cb]"> Full Name</span>
+                      <div className="flex relative items-center">
+                        <FiUser className="absolute left-2 w-6 h-6 mr-2 text-gray-400 hover:shake" onClick={() => setShowPass(!showPass)} />
+                        <input type="text" className="pl-12 w-full p-2 py-3 border bg-white focus:bg-gray-200 rounded-md focus:border-sky-600 focus:outline-sky-600 bg-opacity-80" placeholder="Enter your full name" 
+                        required/>
+                      </div>
+                    </label>
+                    <label className="block mb-4">
+                      <span className="font-semibold mb-2 text-[#2ca1cb]"> Password</span>
+                      <div className="flex relative items-center">
+                        {showPass ? (
+                          <FaEyeSlash className="absolute right-1 w-6 h-6 mr-2 text-gray-500 hover:shake" onClick={() => setShowPass(!showPass)} />
+                        ) : (
+                          <FaEye className="absolute right-1 w-6 h-6 mr-2 text-gray-500 hover:shake" onClick={() => setShowPass(!showPass)} />
+                        )}
+                        <CiLock className="absolute left-2 w-6 h-6 mr-2 text-gray-400 hover:shake" />
+                        <input type={showPass ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)} className="pl-12 w-full p-2 py-3 border bg-white focus:bg-gray-200 rounded-md focus:border-sky-600 focus:outline-sky-600 bg-opacity-80" placeholder="Create your password"
+                        required />
+                      </div>
+                    </label>
+                    <button id="openModalBtn" className="flex justify-center mx-auto rounded-xl px-4 py-2 my-3 overflow-hidden relative group cursor-pointer border-2 font-medium text-lg border-sky-700 w-3/5 text-sky-700 hover:scale-105 transition duration-[900ms]">
+                      <span className="absolute w-64 h-0 transition-all duration-[900ms] origin-center rotate-45 -translate-x-50 bg-sky-700 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+                      <span className="relative text-black transition duration-[900ms] group-hover:text-white ease">
+                        Create an account
+                      </span>
+                    </button>
+
+
+                    <p className="p-4 flex items-center justify-center">
+                      <span className="pe-2 text-black text-sm">Already have an Account?</span>
+                      <Link to="/" className="font-semibold underline text-sm text-black">Login</Link>
+                    </p>
+                    <div className="flex items-center justify-center space-x-4">
+                      <div className="App flex space-x-4">
+                        <Link to="https://www.facebook.com/login" target="_blank" rel="noopener noreferrer">
+                          <FaFacebook className="text-blue-500 hover:shake" size="2em" />
+                        </Link> <Link to="https://twitter.com/login" target="_blank" rel="noopener noreferrer"> <FaTwitter className="text-blue-500 hover:shake" size="2em" /> </Link> <Link to="https://www.instagram.com/accounts/login/" target="_blank" rel="noopener noreferrer"> <FaInstagram className="text-red-500 hover:shake" size="2em" /> </Link> <Link to="https://www.linkedin.com/login" target="_blank" rel="noopener noreferrer"> <FaLinkedin className="text-blue-500 hover:shake" size="2em" /> </Link>
+                      </div>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
-
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email address
-              </label>
-              <div className="mt-1">
-                <input
-                  type="email"
-                  name="email"
-                  autoComplete="email"
-                  required
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <div className="mt-1 relative">
-                <input
-                  type={visible ? "text" : "password"}
-                  name="password"
-                  autoComplete="current-password"
-                  required
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-                {visible ? (
-                  <AiOutlineEye
-                    className="absolute right-2 top-2 cursor-pointer"
-                    size={25}
-                    onClick={() => setVisible(false)}
-                  />
-                ) : (
-                  <AiOutlineEyeInvisible
-                    className="absolute right-2 top-2 cursor-pointer"
-                    size={25}
-                    onClick={() => setVisible(true)}
-                  />
-                )}
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="avatar"
-                className="block text-sm font-medium text-gray-700"
-              ></label>
-              <div className="mt-2 flex items-center">
-                <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
-                  {avatar ? (
-                    <img
-                      src={avatar}
-                      alt="avatar"
-                      className="h-full w-full object-cover rounded-full"
-                    />
-                  ) : (
-                    <RxAvatar className="h-8 w-8" />
-                  )}
-                </span>
-                <label
-                  htmlFor="file-input"
-                  className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
-                >
-                  <span>Upload a file</span>
-                  <input
-                    type="file"
-                    name="avatar"
-                    id="file-input"
-                    accept=".jpg,.jpeg,.png"
-                    onChange={handleFileInputChange}
-                    className="sr-only"
-                  />
-                </label>
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
-              >
-                Submit
-              </button>
-            </div>
-            <div className={`${styles.noramlFlex} w-full`}>
-              <h4>Already have an account?</h4>
-              <Link to="/login" className="text-blue-600 pl-2">
-                Sign In
-              </Link>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default Singup;
+export default Signup;
